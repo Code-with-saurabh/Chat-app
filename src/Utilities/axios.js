1eab20e8395020312ee122558e7c1052150ec0cd
Saurabh <141862550+Code-with-saurabh@users.noreply.github.com>
2026-07-15 17:00:37 +0530

    Update axios.js

--- src/Utilities/axios.js
+++ src/Utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API = import.meta.env.VITE_API_URL;
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API ? `${API}/api` :  "http://localhost:5000/api",
   withCredentials: true,
 });
 
