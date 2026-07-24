275efb7b876a7269f98f5874946286a4055c52df
Saurabh Sharma <idkxyz001@gmail.com>
2026-07-24 20:00:44 +0530

    Delete Files

--- src/Components/Pages/Loding.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loding = () => {
-  return (
-    <div style={{width:"65.56%"}}>Loading Chat...</div>
-  )
-}
-
-export default Loding
--- src/Components/Skeleton/Skeleton.jsx
+++
@@ -1,5 +0,0 @@
-import "./Skeleton.css";
-
-export default function Skeleton({ className }) {
-    return <div className={`skeleton ${className}`} />;
-}
