80388d2f6b116ba56f7b20f1bdb99528651a6458
Saurabh <141862550+Code-with-saurabh@users.noreply.github.com>
2026-07-25 11:01:44 +0530

    Update socket.js

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:5000", {
+const API = import.meta.env.VITE_API_URL;
+export const socket = io(API || "http://localhost:5000", {
     autoConnect: false
 });
