1b412efa3dd981f003c8da409cad1713712a4822
Saurabh <141862550+Code-with-saurabh@users.noreply.github.com>
2026-07-25 13:54:11 +0530

    Update axios.js

--- Frontend/src/Utilities/axios.js
+++ Frontend/src/Utilities/axios.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-// console.log({API})
+console.log({API})
 
 // REQUEST INTERCEPTOR (already hai)
 instance.interceptors.request.use((config) => {
